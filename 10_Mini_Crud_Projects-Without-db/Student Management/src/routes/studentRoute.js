1d6a86b6c5c7cadd46691f4f2dbf6aabcc7b1346
SalmanHossen <hossainsalman873@gmail.com>
2025-10-03 14:53:49 +0600

    complete routing api

--- 10_Mini_Crud_Projects-Without-db/Student Management/src/routes/studentRoute.js
+++ 10_Mini_Crud_Projects-Without-db/Student Management/src/routes/studentRoute.js
@@ -3,5 +3,20 @@ import *as studentController from '../controllers/studentController.js'
 import express from 'express';
 const router=express.Router();
 
+router.post('/:id',studentController.createStudent);
+
+router.get('/',studentController.readALlStudent);
+router.get('/:id',studentController.deleteSingleStudent);
+
+router.put('/:id',studentController.updateStudent);
+router.patch('/:id',studentController.updatePartially);
+
+router.delete('/:id',studentController.deleteSingleStudent);
+router.delete('/',studentController.deleteAllStudent);
+
+router.get('/search',studentController.searchStudent);
+router.get('/sort',studentController.sortingStudent);
+
+
 
 export default router;
