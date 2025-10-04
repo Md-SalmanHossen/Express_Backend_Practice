2d7c4a38c32c235a5cc7e9b8739f9475a726c8d2
SalmanHossen <hossainsalman873@gmail.com>
2025-10-05 00:24:26 +0600

    complete bookeStoreApi.js file

--- 10_Mini_Crud_Projects-Without-db/Book Store/src/routes/bookStoreApi.js
+++ 10_Mini_Crud_Projects-Without-db/Book Store/src/routes/bookStoreApi.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import *as crudController from '../controllers/bookCrudController.js';
+import *as featureController from '../controllers/bookFeatureController.js';
+
 const router =express.Router();
 
+router.post('/',crudController.createBook);
+router.get('/',crudController.readAllBook);
+router.delete('/',crudController.DeleteAllBook);
+
+router.get('/:id',crudController.readSingleBook);
+router.put('/:id',crudController.updateBook);
+router.patch('/:id',crudController.updateBookPartially);
+router.delete('/:id',crudController.deleteSingleBook);
+
+router.get('/action/search',featureController.searchBook);
+router.get('/action/sort',featureController.sortBooks);
+router.get('/action/filter',featureController.filterBooks);
+router.get('/action/paginate',featureController.paginateBook);
+router.get('/action/topRated',featureController.getTopRateBooks);
+router.get('/action/recommendation',featureController.recommendationBooks);
 
 
 export default router;
