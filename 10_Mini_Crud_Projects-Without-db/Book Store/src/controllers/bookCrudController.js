bc1be46c4b73177ef777b0c19ec0b00058627cc8
SalmanHossen <hossainsalman873@gmail.com>
2025-10-05 01:03:48 +0600

    complete read single book controller

--- 10_Mini_Crud_Projects-Without-db/Book Store/src/controllers/bookCrudController.js
+++ 10_Mini_Crud_Projects-Without-db/Book Store/src/controllers/bookCrudController.js
@@ -13,7 +13,17 @@ export const createBook=(req,res)=>{
 }
 export const readSingleBook=(req,res)=>{
    try {
-      
+      const {id}=req.params;
+      const book=books.find((item)=>item.id===id);
+      if(!book){
+         return res.status(404).json({
+            message:`Book with id : ${id} not found`
+         })
+      }
+      return res.status(200).json({
+         message:"Fetched successfully",
+         book_details:book
+      })
    } catch (error) {
       res.status(500).json({
          message:"server error",
