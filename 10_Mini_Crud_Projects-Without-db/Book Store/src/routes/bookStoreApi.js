88f005dd1de59c373c009582ec722a68fa28be48
SalmanHossen <hossainsalman873@gmail.com>
2025-10-05 00:31:14 +0600

    routing folder bug fix

--- 10_Mini_Crud_Projects-Without-db/Book Store/src/routes/bookStoreApi.js
+++ 10_Mini_Crud_Projects-Without-db/Book Store/src/routes/bookStoreApi.js
@@ -4,6 +4,15 @@ import *as featureController from '../controllers/bookFeatureController.js';
 
 const router =express.Router();
 
+// features routing api
+router.get('/action/search',featureController.searchBook);
+router.get('/action/sort',featureController.sortBooks);
+router.get('/action/filter',featureController.filterBooks);
+router.get('/action/paginate',featureController.paginateBook);
+router.get('/action/topRated',featureController.getTopRateBooks);
+router.get('/action/recommendation',featureController.recommendationBooks);
+
+// crud routing api
 router.post('/',crudController.createBook);
 router.get('/',crudController.readAllBook);
 router.delete('/',crudController.DeleteAllBook);
@@ -12,13 +21,6 @@ router.get('/:id',crudController.readSingleBook);
 router.put('/:id',crudController.updateBook);
 router.patch('/:id',crudController.updateBookPartially);
 router.delete('/:id',crudController.deleteSingleBook);
-
-router.get('/action/search',featureController.searchBook);
-router.get('/action/sort',featureController.sortBooks);
-router.get('/action/filter',featureController.filterBooks);
-router.get('/action/paginate',featureController.paginateBook);
-router.get('/action/topRated',featureController.getTopRateBooks);
-router.get('/action/recommendation',featureController.recommendationBooks);
 
 
 export default router;
