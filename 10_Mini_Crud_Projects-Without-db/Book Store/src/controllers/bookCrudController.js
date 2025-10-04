fef041e1f56f60005015c2c32395c304d98cb10f
SalmanHossen <hossainsalman873@gmail.com>
2025-10-04 23:21:24 +0600

    skeliton done crudBookController.js

---
+++ 10_Mini_Crud_Projects-Without-db/Book Store/src/controllers/bookCrudController.js
@@ -0,0 +1,73 @@
+
+export const createBook=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"server error",
+         error:error.message
+      })
+   }
+}
+export const readSingleBook=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"server error",
+         error:error.message
+      })
+   }
+}
+export const readAllBook=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"server error",
+         error:error.message
+      })
+   }
+}
+export const updateBook=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"server error",
+         error:error.message
+      })
+   }
+}
+export const updateBookPartially=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"server error",
+         error:error.message
+      })
+   }
+}
+export const deleteSingleBook=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"server error",
+         error:error.message
+      })
+   }
+}
+export const DeleteAllBook=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"server error",
+         error:error.message
+      })
+   }
+}
+
+export default crudBookController.js;
