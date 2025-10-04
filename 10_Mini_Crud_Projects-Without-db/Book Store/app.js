d334e2fb76ec7fe01869aa7f191f5d5befc663de
SalmanHossen <hossainsalman873@gmail.com>
2025-10-04 21:46:55 +0600

    complete config file

---
+++ 10_Mini_Crud_Projects-Without-db/Book Store/app.js
@@ -0,0 +1,11 @@
+
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import hpp from 'hpp';
+import helmet from 'helmet';
+
+
+
+export default app;
