57d791f258d394f73655c3634fb5e214c4b8096a
SalmanHossen <hossainsalman873@gmail.com>
2025-10-04 22:00:09 +0600

    add routing api

--- 10_Mini_Crud_Projects-Without-db/Book Store/app.js
+++ 10_Mini_Crud_Projects-Without-db/Book Store/app.js
@@ -8,5 +8,22 @@ import helmet from 'helmet';
 
 const app=express();
 
+//security middleware
+app.use(cors());
+app.use(hpp());
+app.use(helmet());
+app.use(cookieParser());
+const ratelimiter=rateLimit({
+   windowMs:15*60*1000,
+   max:1000,
+   standardHeaders:true,
+});
+app.use(ratelimiter);
+
+//body parser 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
 
 export default app;
--- 10_Mini_Crud_Projects-Without-db/Book Store/index.js
+++ 10_Mini_Crud_Projects-Without-db/Book Store/index.js
@@ -4,6 +4,14 @@ dotenv.config();
 
 const PORT=process.env.PORT;
 
+//routing api
+app.get('/',(req,res)=>{
+   res.status(200).json({
+      page:"Home page",
+      message:"You are welcome"
+   })
+})
+;
 app.listen(PORT,()=>{
    console.log(`Server running on port http//:localhost:${PORT}`);
 })
