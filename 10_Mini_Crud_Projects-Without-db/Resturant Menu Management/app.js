05e683a4c2b1e8dd3f4d73244d72607ccfed00c4
SalmanHossen <hossainsalman873@gmail.com>
2025-10-06 18:22:26 +0600

    security middleware impletented

---
+++ 10_Mini_Crud_Projects-Without-db/Resturant Menu Management/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import hpp from 'hpp';
+import helmet from 'helmet';
+
+const app=express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(helmet());
+app.use(hpp());
+const limiter=rateLimit({
+   windowMs:15*60*1000,
+   max:1000,
+   standardHeaders:true
+});
+app.use(limiter());
+
+
+export default app;
