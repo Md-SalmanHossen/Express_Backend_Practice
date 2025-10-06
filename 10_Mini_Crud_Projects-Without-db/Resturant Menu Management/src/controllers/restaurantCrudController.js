f87dff623fe6a3ca0cc8624af0dd8e70da8d665c
SalmanHossen <hossainsalman873@gmail.com>
2025-10-06 22:22:10 +0600

    crud controller and routing setup complete

--- 10_Mini_Crud_Projects-Without-db/Resturant Menu Management/src/controllers/restaurantCrudController.js
+++ 10_Mini_Crud_Projects-Without-db/Resturant Menu Management/src/controllers/restaurantCrudController.js
@@ -1,2 +1,72 @@
-import foodMenu from '../database/db';
+import foodMenu from '../database/db.js';
 
+export const createFoodMenu=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"Server error",
+         error:error.message
+      })
+   }
+}
+export const readAllFoodMenu=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"Server error",
+         error:error.message
+      })
+   }
+}
+export const readSingleFood=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"Server error",
+         error:error.message
+      })
+   }
+}
+export const updateFoodMenu=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"Server error",
+         error:error.message
+      })
+   }
+}
+export const updatePartialFoodMenu=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"Server error",
+         error:error.message
+      })
+   }
+}
+export const deleteSingleFoodMenu=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"Server error",
+         error:error.message
+      })
+   }
+}
+export const deleteFullFoodMenu=(req,res)=>{
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         message:"Server error",
+         error:error.message
+      })
+   }
+}
--- 10_Mini_Crud_Projects-Without-db/Resturant Menu Management/src/routes/restaurantApi.js
+++ 10_Mini_Crud_Projects-Without-db/Resturant Menu Management/src/routes/restaurantApi.js
@@ -1,7 +1,20 @@
-
+import *as restaurantController from '../controllers/restaurantCrudController.js'
 import express from 'express';
 const router=express.Router();
 
+
+
+
+
+// 1. CRUD Routes
+router.post('/',restaurantController.createFoodMenu);
+router.get('/',restaurantController.readAllFoodMenu);
+router.delete('/',restaurantController.deleteFullFoodMenu );
+
+router.get('/:id',restaurantController.readSingleFood);
+router.put('/:id',restaurantController.updateFoodMenu);
+router.patch('/:id',restaurantController.updatePartialFoodMenu );
+router.delete('/:id',restaurantController.deleteSingleFoodMenu );
 
 
 
