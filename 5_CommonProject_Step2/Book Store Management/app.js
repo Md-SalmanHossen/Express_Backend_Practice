a53e0148e32c506ee2f911e9cef150e36be5be15
SalmanHossen <hossainsalman873@gmail.com>
2025-10-09 23:36:04 +0600

    setup security and mongoose connection

--- 5_CommonProject_Step2/Book Store Management/app.js
+++ 5_CommonProject_Step2/Book Store Management/app.js
@@ -3,8 +3,11 @@ import rateLimit from 'express-rate-limit';
 import cors from 'cors';
 import hpp from 'hpp';
 import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import notFound from './src/middlewares/notFound.js';
+
 
 const app=express();
 
@@ -20,7 +23,9 @@ app.use(rateLimiter);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+app.use(mongoSanitize());
 
 app.use(notFound)
 
--- 5_CommonProject_Step2/Book Store Management/index.js
+++ 5_CommonProject_Step2/Book Store Management/index.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
 import dotenv from 'dotenv';
+import connectDB from "./src/utils/dbConnection.js";
+
 dotenv.config();
+connectDB();
 
 const PORT=process.env.PORT || 5555;
 
