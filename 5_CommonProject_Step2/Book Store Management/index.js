df72c97bcc8c81b4cb6775e5162d40482f48c037
SalmanHossen <hossainsalman873@gmail.com>
2025-10-09 23:20:47 +0600

    done mongoose connection

--- 5_CommonProject_Step2/Book Store Management/index.js
+++ 5_CommonProject_Step2/Book Store Management/index.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT=process.env.PORT ||1111;
+const PORT=process.env.PORT || 5555;
 
 app.listen(PORT,()=>{
    console.log(`Server is running on ${PORT}`);
---
+++ 5_CommonProject_Step2/Book Store Management/src/utils/dbConnection.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+
+const connectURI=process.env.MONGO_URI_LOCAL;
+
+const connectDB=async()=>{
+   try {
+      const conn=await mongoose.connect(connectURI);
+      console.log(`MongoDb connected : ${conn.connection.host}`);
+   } catch (error) {
+      console.error(`Error connecting to MongoDB Local : ${error.message}`)
+   }
+}
+
+export default connectDB;
