bc44d71fb8a4369d16701eb348b3afffec7a8f43
SalmanHossen <hossainsalman873@gmail.com>
2025-10-10 19:09:46 +0600

    create food menu schema

---
+++ 5_CommonProject_Step2/Book Store Management/src/models/foodMenu.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const foodMenuSchema=new mongoose.Schema({
+   name:{
+      type:String,
+      required:[true , 'Please provided food name'],
+      trim:true,
+      unique:true
+   },
+   description:{
+      type:String,
+      required:[true,'Please provide description']
+   },
+   price:{
+      type:Number,
+      required:[true,'Please provide a price']
+   },
+   category:{
+      type:String,
+      required:[true,'Please provide a category']
+   },
+   imageUrl:{
+      type:String,
+      default:'no-photo.jpg'
+   },
+   rating:{
+      type:Number,
+      min:1,
+      max:5,
+      default:4.5
+   },
+   inStock:{
+      type:Boolean,
+      default:true,
+   },
+   isFeatured:{
+      type:Boolean,
+      default:false
+   }
+},{
+   timesStamps:true
+})
+
+const FoodMenu=mongoose.model('foodMenu',foodMenuSchema);
+export default FoodMenu;
