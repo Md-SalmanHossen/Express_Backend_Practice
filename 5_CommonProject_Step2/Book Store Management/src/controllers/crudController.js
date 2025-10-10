2ef7a5894c3453f30e6370b57149a00c39c6a874
SalmanHossen <hossainsalman873@gmail.com>
2025-10-10 19:48:07 +0600

    complete create controller

---
+++ 5_CommonProject_Step2/Book Store Management/src/controllers/crudController.js
@@ -0,0 +1,19 @@
+import FoodMenu from "../models/foodMenu.js";
+
+export const createFoodMenu=async(req,res)=>{
+   try {
+      const newFood=await FoodMenu.create(req.body);
+      res.status(201).json({
+         success:true,
+         message:true,
+         data:newFood
+      })
+   } catch (error) {
+      res.status(200).json({
+         success:false,
+         message:"Failed to create item",
+         error:error.message,
+      })
+   }
+}
+
