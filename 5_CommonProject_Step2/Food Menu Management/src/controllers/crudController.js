5fd2fddf05ff2bc6d9d50b55824d840fdc000977
SalmanHossen <hossainsalman873@gmail.com>
2025-10-13 21:02:21 +0600

    fix connection issues

--- 5_CommonProject_Step2/Food Menu Management/src/controllers/crudController.js
+++ 5_CommonProject_Step2/Food Menu Management/src/controllers/crudController.js
@@ -108,3 +108,4 @@ export const updateFoodMenu=async(req,res)=>{
       })
    }
 }
+
--- 5_CommonProject_Step2/Food Menu Management/src/routes/foodMenuApi.js
+++ 5_CommonProject_Step2/Food Menu Management/src/routes/foodMenuApi.js
@@ -5,8 +5,8 @@ const router=express.Router();
 router.post('/',crudController.createFoodMenu);
 router.get('/',crudController.readAllFoodMenu);
 router.get('/:id',crudController.readSingleFoodMenu);
-console.log(crudController.updateFoodMenu);
 router.put('/:id',crudController.updateFoodMenu);
+
 
 
 export default router;
