d8dcf2851fa52ceeb249c2c90011293f0e55a54e
SalmanHossen <hossainsalman873@gmail.com>
2025-10-14 22:27:08 +0600

    add error handler

--- 5_CommonProject_Step2/Food Menu Management/src/controllers/featureController.js
+++ 5_CommonProject_Step2/Food Menu Management/src/controllers/featureController.js
@@ -1,7 +1,15 @@
-import FoodMenu from "../models/foodMenu";
+import FoodMenu from "../models/foodMenu.js";
 
 export const searchController=async(req,res)=>{
    try {
+      const {searchQuery}=req.query;
+      if(!searchQuery){
+         return res.status(404).json({
+            status:"Not Found",
+            message:"Server not found"
+         })
+      }
+
       
    } catch (error) {
       res.status(500).json({
