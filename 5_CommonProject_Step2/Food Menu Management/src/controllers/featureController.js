d3f893b672b725ffa578fbdf5babfabba73b69ee
SalmanHossen <hossainsalman873@gmail.com>
2025-10-15 11:01:47 +0600

    create feature controller

--- 5_CommonProject_Step2/Food Menu Management/src/controllers/featureController.js
+++ 5_CommonProject_Step2/Food Menu Management/src/controllers/featureController.js
@@ -35,5 +35,13 @@ export const searchController=async(req,res)=>{
 }
 
 export const sortController=async(req,res)=>{
-   
+   try {
+      
+   } catch (error) {
+      res.status(500).json({
+         status:"error",
+         message:"Server internal error",
+         error:error.message
+      })
+   }
 }
