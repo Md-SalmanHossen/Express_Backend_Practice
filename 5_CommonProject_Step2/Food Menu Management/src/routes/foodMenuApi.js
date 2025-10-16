fe25a32e08e1c63cc8245299b7380d29e1d559e7
SalmanHossen <hossainsalman873@gmail.com>
2025-10-16 23:06:26 +0600

    feature controller filter done

--- 5_CommonProject_Step2/Food Menu Management/src/routes/foodMenuApi.js
+++ 5_CommonProject_Step2/Food Menu Management/src/routes/foodMenuApi.js
@@ -6,6 +6,7 @@ const router=express.Router();
 
 router.get('/action/search',featureController.searchController);
 router.get('/action/sort',featureController.sortController);
+router.get('/action/filter',featureController.filterController);
 
 router.post('/',crudController.createFoodMenu);
 router.get('/',crudController.readAllFoodMenu);
