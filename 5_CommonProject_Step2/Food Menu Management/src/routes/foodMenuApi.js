fb275234ce0fe861fab726c7d45e2f8f5fa10a5e
SalmanHossen <hossainsalman873@gmail.com>
2025-10-17 11:52:35 +0600

    route api setup

--- 5_CommonProject_Step2/Food Menu Management/src/routes/foodMenuApi.js
+++ 5_CommonProject_Step2/Food Menu Management/src/routes/foodMenuApi.js
@@ -7,6 +7,7 @@ const router=express.Router();
 router.get('/action/search',featureController.searchController);
 router.get('/action/sort',featureController.sortController);
 router.get('/action/filter',featureController.filterController);
+router.get('/action/recommendation',featureController.recommendationFood);
 
 router.post('/',crudController.createFoodMenu);
 router.get('/',crudController.readAllFoodMenu);
