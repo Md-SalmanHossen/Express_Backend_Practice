61110cdf42930f3d53cb658d3a8a8293910b69ae
SalmanHossen <hossainsalman873@gmail.com>
2025-10-20 19:17:32 +0600

    import express rate limit

--- Separately Practice/1_multer/app.js
+++ Separately Practice/1_multer/app.js
@@ -2,6 +2,7 @@ import express  from 'express';
 import hpp from 'hpp';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import rateLimit from  'express-rate-limit';
 
 import routeNotFound from './src/middlewares/routeNotFound.js';
 const app=express();
