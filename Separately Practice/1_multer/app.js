8e4c6ec474c0704a9c6cecfa759966c7434add09
SalmanHossen <hossainsalman873@gmail.com>
2025-10-20 19:14:35 +0600

    import cookie parser

--- Separately Practice/1_multer/app.js
+++ Separately Practice/1_multer/app.js
@@ -1,7 +1,7 @@
 import express  from 'express';
 import hpp from 'hpp';
 import helmet from 'helmet';
-
+import cookieParser from 'cookie-parser';
 
 import routeNotFound from './src/middlewares/routeNotFound.js';
 const app=express();
