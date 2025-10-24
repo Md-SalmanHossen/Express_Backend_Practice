868874d2cca291407dff4a46760f81b5fb75081c
SalmanHossen <hossainsalman873@gmail.com>
2025-10-24 11:40:30 +0600

    express rate limit import

--- 5_CommonProject_Step2/Student Management/app.js
+++ 5_CommonProject_Step2/Student Management/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import hpp from 'hpp';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 
 const app=express();
@@ -10,6 +11,7 @@ const app=express();
 app.use(hpp());
 app.use(helmet());
 app.use(cookieParser());
+
 
 
 export default app;
