8c45174f349e189ec083ef1cc20a9ec2395e61c5
SalmanHossen <hossainsalman873@gmail.com>
2025-10-24 11:42:08 +0600

    use rate limiter as a middleare

--- 5_CommonProject_Step2/Student Management/app.js
+++ 5_CommonProject_Step2/Student Management/app.js
@@ -11,7 +11,11 @@ const app=express();
 app.use(hpp());
 app.use(helmet());
 app.use(cookieParser());
-
+const limiter=rateLimit({
+   windowMs:60*60*1000,
+   max:1000,
+});
+app.use(limiter);
 
 
 export default app;
