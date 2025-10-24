7719559ecb039f0ea95189b0b6399c420f745879
SalmanHossen <hossainsalman873@gmail.com>
2025-10-24 11:35:19 +0600

    import cookie parser

--- 5_CommonProject_Step2/Student Management/app.js
+++ 5_CommonProject_Step2/Student Management/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import hpp from 'hpp';
+import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+
+
 const app=express();
 
 
 app.use(hpp());
 app.use(helmet());
+//app.use(cookieParser());
 
 
 export default app;
