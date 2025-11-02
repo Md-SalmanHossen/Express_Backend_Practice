634a832cd9ffa5ab101e3bc2e7b37b29cbc8a1c8
SalmanHossen <hossainsalman873@gmail.com>
2025-11-02 13:40:55 +0600

    create user model

--- 5 Complete Projects Step4/Blog Api with comments/src/middleware/authMiddleware.js
+++ 5 Complete Projects Step4/Blog Api with comments/src/middleware/authMiddleware.js
@@ -2,6 +2,7 @@ import {jwt} from 'jsonwebtoken';
 
 const authMiddleware=(req,res,next)=>{
    try {
+      
       const token=req.headers.authorization?.split(' ')[1];
       if(!token) return res.status(404).json({
          message:"Token missing",
