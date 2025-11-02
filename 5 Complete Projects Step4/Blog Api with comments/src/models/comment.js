ae06e5fc698fca8369a770f6be1a9773b89be4c3
SalmanHossen <hossainsalman873@gmail.com>
2025-11-02 14:12:51 +0600

    create comment model

---
+++ 5 Complete Projects Step4/Blog Api with comments/src/models/comment.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const commentSchema=mongoose.Schema({
+   commentText:String,
+   blog:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'blog'
+   },
+   user:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'user'
+   }
+})
+
+export default mongoose.model('comment',commentSchema);
