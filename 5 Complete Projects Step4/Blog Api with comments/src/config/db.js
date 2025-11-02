80b0d67e52bb2cf628d2cc4ca9fa55b38342c26b
SalmanHossen <hossainsalman873@gmail.com>
2025-11-02 11:59:04 +0600

    create db conenction file and import

--- 5 Complete Projects Step4/Blog Api with comments/src/config/db.js
+++ 5 Complete Projects Step4/Blog Api with comments/src/config/db.js
@@ -4,9 +4,11 @@ import mongoose from "mongoose";
 const connectDB=async ()=>{
    try {
       mongoose.connect(process.env.MONG_URI);
-      
+
    } catch (error) {
       console.log('MongoDB Error: ',error);
       process.exit(1);
    }
 }
+
+export default connectDB;
