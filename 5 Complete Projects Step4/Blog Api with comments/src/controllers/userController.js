8876688d55f407cb58f277099f0cb05bf5dd0c78
SalmanHossen <hossainsalman873@gmail.com>
2025-11-02 19:00:33 +0600

    user controller done

--- 5 Complete Projects Step4/Blog Api with comments/src/controllers/userController.js
+++ 5 Complete Projects Step4/Blog Api with comments/src/controllers/userController.js
@@ -1,6 +1,9 @@
 import bcrypt from 'bcryptjs';
-import user from '../models/user';
- 
+import user from '../models/user.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 export const register=async (req,res)=>{
    try {
       const {name,email,password}=req.body;
@@ -22,5 +25,40 @@ export const register=async (req,res)=>{
          message:"Server error",
          error:error.message
       })
+   }
+}
+
+export const login=async(req,res)=>{
+   try {
+
+      const {email,password}=req.body;
+      const user=await user.findOne({email});
+
+      if(!user) return res.status(404).json({
+         message:"User not found"
+      })
+      
+      const match=bcrypt.compare(password,user.password);
+      if(!match) return res.status(404).json({
+         message:"Wrong password",
+      });
+
+      const token=jwt.sign(
+         { 
+            userId:user._id
+         },
+         process.env.JWT_SECRET
+     );
+
+     res.status(200).json({
+      message:"Login Success",
+      token
+     });
+
+   } catch (error) {
+      res.status(500).json({
+         message:"Server error",
+         error:error.message
+      });
    }
 }
