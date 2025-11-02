a5f512eda97adcd2e1537d32629df0f0d9967d1f
SalmanHossen <hossainsalman873@gmail.com>
2025-11-02 13:35:07 +0600

    auth middleware done

---
+++ 5 Complete Projects Step4/Blog Api with comments/src/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import {jwt} from 'jsonwebtoken';
+
+const authMiddleware=(req,res,next)=>{
+   try {
+      const token=req.headers.authorization?.split(' ')[1];
+      if(!token) return res.status(404).json({
+         message:"Token missing",
+      });
+      const decode= jwt.verify(token,process.env.JWT_SECRET);
+
+      console.log(req);
+      console.log(decode);
+
+      req.user=decode.userId;
+
+      next();
+
+   } catch (error) {
+      res.status(500).json({
+         message:"Invalid Token",
+         error:error.message
+      })
+   }
+}
+
+export default authMiddleware;
