60de80c13ea6593159a6f250ddca0b781c2e68a0
SalmanHossen <hossainsalman873@gmail.com>
2025-11-03 11:30:15 +0600

    fix code status for authMiddleware

--- 5 Complete Projects Step4/Blog Api with comments/src/middleware/authMiddleware.js
+++ 5 Complete Projects Step4/Blog Api with comments/src/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const authMiddleware=(req,res,next)=>{
    try {
       
       const token=req.headers.authorization?.split(' ')[1];
-      if(!token) return res.status(404).json({
+      if(!token) return res.status(401).json({
          message:"Token missing",
       });
       const decode= jwt.verify(token,process.env.JWT_SECRET);
