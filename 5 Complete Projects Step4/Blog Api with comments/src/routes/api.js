75dac1f6518eafb6c31d7f94a44ace7db0432e18
SalmanHossen <hossainsalman873@gmail.com>
2025-11-03 10:05:54 +0600

    create blog

--- 5 Complete Projects Step4/Blog Api with comments/src/routes/api.js
+++ 5 Complete Projects Step4/Blog Api with comments/src/routes/api.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import { register,login } from './../controllers/userController.js';
+import authMiddleware from './../middleware/authMiddleware.js';
+import { createBlog } from './../controllers/blogController.js';
+
 const router=express.Router();
  
-
+   
 router.post('/register',register);
 router.post('/register',login );
+
+router.post('/',authMiddleware,createBlog);
