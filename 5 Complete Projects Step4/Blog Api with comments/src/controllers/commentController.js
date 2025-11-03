347dc7def366981ecbc1c7eafff7ea036611c3b3
SalmanHossen <hossainsalman873@gmail.com>
2025-11-03 09:58:04 +0600

    create add comments controller

--- 5 Complete Projects Step4/Blog Api with comments/src/controllers/commentController.js
+++ 5 Complete Projects Step4/Blog Api with comments/src/controllers/commentController.js
@@ -2,7 +2,7 @@ import comment from "../models/comment.js";
 
 export const addComment=async(req ,res)=>{
    try {
-      const {comment}=req.body;
+      const {commentText,blogId}=req.body;
 
       const addComment=await comment.create({
          commentText,
@@ -14,8 +14,29 @@ export const addComment=async(req ,res)=>{
          message:"Comment add successfully",
          addComment
       })
+
    } catch (error) {
       res.status(200).json({
+         message:"Server error",
+         error:error.message
+      })
+   }
+}
+
+
+export const getBlogWithComments=async(req , res)=>{
+   try {
+      const {blogId}=req.params;
+      const comments=await comment.find(
+         {blog:blogId}.populate('user','name')
+      );
+
+      res.status(200).json({
+         message:"comments successfully",
+         comments
+      })
+   } catch (error) {
+      res.status(500).json({
          message:"Server error",
          error:error.message
       })
