fe8a9b1203d97115985decf31f7554faecdc7bd4
SalmanHossen <hossainsalman873@gmail.com>
2025-11-04 11:07:16 +0600

    create blog controller

--- 5 Complete Projects Step4/Portfolio/src/controllers/blog.controller.js
+++ 5 Complete Projects Step4/Portfolio/src/controllers/blog.controller.js
@@ -1,6 +1,22 @@
+import Blog from '../models/userModel.js';
 
 export const createBlog=async(req,res)=>{
    try {
+
+      const {title,img,category,description,short_description}=req.body;
+      const createBlog=await Blog.create({
+         title,
+         img,
+         category,
+         description,
+         short_description
+      })
+
+      res.status(201).json({
+         success:true,
+         message:"Blog created successfully",
+         data:createBlog
+      });
       
    } catch (error) {
       res.status(500).json({
