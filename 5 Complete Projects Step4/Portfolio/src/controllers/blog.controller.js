744fb20a529e509283d04d36fc6a206cff16a6db
SalmanHossen <hossainsalman873@gmail.com>
2025-11-04 12:33:48 +0600

    crate get all blogs controller

--- 5 Complete Projects Step4/Portfolio/src/controllers/blog.controller.js
+++ 5 Complete Projects Step4/Portfolio/src/controllers/blog.controller.js
@@ -17,11 +17,49 @@ export const createBlog=async(req,res)=>{
          message:"Blog created successfully",
          data:createBlog
       });
-      
+
    } catch (error) {
       res.status(500).json({
          message:"Server error",
          error:error.message,
+      })
+   }
+}
+
+export const getAllBlog=async(req,res)=>{
+   try{
+      const facetStage={
+         $facet:{
+            totalCOunt:[{$count:"count"}],
+            blogs:[
+               {$sort: {createdAt: -1}},
+               {
+                  $project:{
+                     title:1,
+                     img:1,
+                     category:1,
+                     description:1,
+                     short_description:1,
+                  }
+               } 
+            ]
+         }
+      }; 
+      const getAll=await Blog.aggregate(facetStage) ;
+       
+      res.status(200).json({
+         success:true,
+         message:"Fetched data all successfully",
+         error:error.message,
+         total_blog:getAll.length,
+         data:getAll
+      });
+      
+   }catch(error){
+      res.status(500).json({
+         success:false,
+         message:"Fetched to failed and occur error",
+         error:error.message
       })
    }
 }
