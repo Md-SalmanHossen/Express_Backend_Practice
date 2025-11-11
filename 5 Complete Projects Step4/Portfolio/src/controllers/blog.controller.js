ed4ae7ce8f8ceefca6d81ee7fac54139202728a0
SalmanHossen <hossainsalman873@gmail.com>
2025-11-11 18:23:22 +0600

    create and export db.js config file

--- 5 Complete Projects Step4/Portfolio/src/controllers/blog.controller.js
+++ 5 Complete Projects Step4/Portfolio/src/controllers/blog.controller.js
@@ -1,65 +1,61 @@
-import Blog from '../models/userModel.js';
+import Blog from "../models/userModel.js";
 
-export const createBlog=async(req,res)=>{
-   try {
+export const createBlog = async (req, res) => {
+  try {
+    const { title, img, category, description, short_description } = req.body;
+    const createBlog = await Blog.create({
+      title,
+      img,
+      category,
+      description,
+      short_description,
+    });
 
-      const {title,img,category,description,short_description}=req.body;
-      const createBlog=await Blog.create({
-         title,
-         img,
-         category,
-         description,
-         short_description
-      })
+    res.status(201).json({
+      success: true,
+      message: "Blog created successfully",
+      data: createBlog,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
 
-      res.status(201).json({
-         success:true,
-         message:"Blog created successfully",
-         data:createBlog
-      });
+export const getAllBlog = async (req, res) => {
+  try {
+    const facetStage = {
+      $facet: {
+        totalCount: [{ $count: "count" }],
+        blogs: [
+          { $sort: { createdAt: -1 } },
+          {
+            $project: {
+              title: 1,
+              img: 1,
+              category: 1,
+              description: 1,
+              short_description: 1,
+            },
+          },
+        ],
+      },
+    };
+    const getAll = await Blog.aggregate(facetStage);
 
-   } catch (error) {
-      res.status(500).json({
-         message:"Server error",
-         error:error.message,
-      })
-   }
-}
-
-export const getAllBlog=async(req,res)=>{
-   try{
-      const facetStage={
-         $facet:{
-            totalCOunt:[{$count:"count"}],
-            blogs:[
-               {$sort: {createdAt: -1}},
-               {
-                  $project:{
-                     title:1,
-                     img:1,
-                     category:1,
-                     description:1,
-                     short_description:1,
-                  }
-               } 
-            ]
-         }
-      }; 
-      const getAll=await Blog.aggregate(facetStage) ;
-       
-      res.status(200).json({
-         success:true,
-         message:"Fetched data all successfully",
-         error:error.message,
-         total_blogs:getAll.length,
-         data:getAll
-      });
-
-   }catch(error){
-      res.status(500).json({
-         success:false,
-         message:"Fetched to failed and occur error",
-         error:error.message
-      })
-   }
-}
+    res.status(200).json({
+      success: true,
+      message: "Fetched data all successfully",
+      total_blogs: getAll.length,
+      data: getAll,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Fetched to failed and occur error",
+      error: error.message,
+    });
+  }
+};
---
+++ 5_Auth_Practice_With_Project_Step3/Auth_Blog/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv  from 'dotenv';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+
+
+const app=express();
+dotenv.config();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use(express.json);
+app.use(express.urlencoded({extended:true}));
+
+
+
+export default app;
