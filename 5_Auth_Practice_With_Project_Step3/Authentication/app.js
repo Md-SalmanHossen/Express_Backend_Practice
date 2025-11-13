909a38a5706b623b2bf853d39187a486e5020ded
SalmanHossen <hossainsalman873@gmail.com>
2025-11-13 21:09:20 +0600

    use necessary middleare in the app

--- 5_Auth_Practice_With_Project_Step3/Authentication/app.js
+++ 5_Auth_Practice_With_Project_Step3/Authentication/app.js
@@ -1,10 +1,19 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv';
-
+import cors from 'cors';
+import connectDB from './src/config/connectDB.config.js';
 
 const app=express();
 dotenv.config();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+connectDB();
 
 
 export default app;
