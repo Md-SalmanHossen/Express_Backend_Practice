b624a037e300619512325e6de3381b4b75efbf31
SalmanHossen <hossainsalman873@gmail.com>
2025-11-15 21:59:40 +0600

    complete logout controller

--- 5_Auth_Practice_With_Project_Step3/Authentication/src/controllers/user.controller.js
+++ 5_Auth_Practice_With_Project_Step3/Authentication/src/controllers/user.controller.js
@@ -147,6 +147,15 @@ export const updateProfile=async(req ,res )=>{
 
 export const logout=async(req ,res )=>{
    try {
+      res.cookie('token','',{
+         httpOnly:true,
+         expires:new Date(0)
+      });
+
+      res.status(200).json({
+         status:"success",
+         message:'User logout'
+      });
       
    } catch (error) {
       res.status(500).json({
