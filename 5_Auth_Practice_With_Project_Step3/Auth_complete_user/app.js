aad114e21bb2decbe914eab6df81fb92368d6f0b
SalmanHossen <hossainsalman873@gmail.com>
2025-11-16 20:29:28 +0600

    create app.js

--- 5_Auth_Practice_With_Project_Step3/Auth_complete_user/app.js
+++ 5_Auth_Practice_With_Project_Step3/Auth_complete_user/app.js
@@ -1,6 +1,26 @@
-import mongoose from "mongoose";
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-const connectDB=await mongoose.connect({
-   
-});
+import connectDB from './src/config/connectDB.config.js';
+import routeHandler from './src/middlewares/route_handler.middleware.js';
+import router from './src/routes/user.routes.js';
 
+const app=express();
+dotenv.config();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+connectDB();
+
+app.use('/auth/api/v1',router);
+
+app.use(routeHandler);
+
+
+export default app;
