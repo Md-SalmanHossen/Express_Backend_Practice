9b39d844dc42780a332810f029446d296bacaf57
SalmanHossen <hossainsalman873@gmail.com>
2025-11-19 23:22:05 +0600

    fix the issues and hashing the pass before login

--- 5_Auth_Practice_With_Project_Step3/Auth_Reset_Pass/src/controllers/user.controller.js
+++ 5_Auth_Practice_With_Project_Step3/Auth_Reset_Pass/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import UsersModel from './../models/Users.Model.js';
-
+import  bcrypt, { genSalt }  from 'bcryptjs';
 export const signup=async (req,res)=>{
    try{
       const {email,firstName,lastName,mobile,password}=req.body;
@@ -11,13 +11,15 @@ export const signup=async (req,res)=>{
             message:'Email already exists'
          })
       }
+      const salt=await bcrypt.genSalt(10)
+      const hashedPass=await bcrypt.hash(password,salt);
 
       const result=await UsersModel.create({
          email,
          firstName,
          lastName,
          mobile,
-         password
+         password:hashedPass
       });
 
       res.status(201).json({
@@ -37,11 +39,33 @@ export const signup=async (req,res)=>{
 
 export const login=async (req,res)=>{
    try{
-       
+       const {email,password}=req.body;
+       const user=await UsersModel.findOne({email});
+
+       if(!user){
+         return res.status(404).json({
+            status:"fail",
+            message:'User not found'
+         })
+       }
+
+       if(user.password!==password){
+         return res.json({
+            status:'fail',
+            message:'Wrong email or password'
+         })
+       };
+
+      res.status(200).json({
+         status:'Success',
+         message:'Login successfully',
+         user_data:user
+      });
+
    }catch (error) {
        res.json({
         status:"fail",
-        message:'Server error occur during create tasks',
+        message:'Server error occur during login',
         error:error.message
      })
    }
