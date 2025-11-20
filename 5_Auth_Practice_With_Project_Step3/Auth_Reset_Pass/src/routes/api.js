ec84ad4ff959dad3e53cb00d689a07a48c9724bf
SalmanHossen <hossainsalman873@gmail.com>
2025-11-20 12:07:55 +0600

    create api end point

--- 5_Auth_Practice_With_Project_Step3/Auth_Reset_Pass/src/routes/api.js
+++ 5_Auth_Practice_With_Project_Step3/Auth_Reset_Pass/src/routes/api.js
@@ -1,9 +1,17 @@
 import express from "express";
 import *as user from '../controllers/user.controller.js'
+import auth from '../middlewares/auth.middleware.js'
 const router=express.Router();
 
 router.post('/signup',user.signup);
 router.post('/login',user.login);
+
+router.get('profile',auth, user.profileDetails);
+router.put('profile',auth, user.ProfileUpdate);
+router.delete('profile',auth, user.profileDelete);
+router.get('profile',auth, user.logout);
+
+
 
 
 export default router;
