137234167b58bf09946e5aa1f460f55a9cb6f261
SalmanHossen <hossainsalman873@gmail.com>
2025-11-22 23:38:11 +0600

    create user model

---
+++ 5 Complete Projects Step4/Portfolio/src/models/User.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema =new mongoose.Schema(
+  {
+    name:{ 
+      type: String, 
+      required: [true, "Name is required"] 
+   },
+    email:{
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Invalid email"],
+    },
+    password:{
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+    role:{ 
+      type: String, 
+      enum: ["admin", "client"], 
+      default: "client" 
+   },
+    avatarUrl:{
+      type:String,
+      default:""
+   },
+    bio: String,
+    isEmailVerified:{
+      type: Boolean, 
+      default: false 
+   },
+   resetPasswordToken:{
+      type:String,
+      default:null,
+   },
+   resetPasswordExpired:{
+      type:Date,
+      default:null
+   },
+    refreshTokens: [String], // stores hashed refresh tokens for revocation
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User',userSchema);
