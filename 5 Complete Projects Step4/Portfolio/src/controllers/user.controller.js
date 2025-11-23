b402cf2f084f7478e908a47c9e194e7d37831523
SalmanHossen <hossainsalman873@gmail.com>
2025-11-23 13:41:26 +0600

    login controller create

--- 5 Complete Projects Step4/Portfolio/src/controllers/user.controller.js
+++ 5 Complete Projects Step4/Portfolio/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 
 import User from '../models/User.model.js';
 import sendEmail from './../utils/email.utils.js';
+import { jwt } from 'jsonwebtoken';
 
 
 
@@ -106,7 +107,38 @@ export const verifyEmail=async(req ,res)=>{
 
 export const login=async(req ,res)=>{
    try {
-      
+
+      const {email,password}=req.body;
+      if(!email || !password){
+         return res.status(400).json({
+            message:'All fields are require',
+         })
+      };
+
+      const user=await User.findOne({email}).select('-password');
+      if(!user){
+         return res.status(404).json({
+            message:'User not found'
+         })
+      };
+
+      const matchedPass=await bcrypt.compare(password,user.password);
+      if(!matchedPass){
+         return res.status(400).json({
+            message:"Wrong email or password"
+         });
+      }
+
+      const token=jwt.sign(
+         {id:user._id},
+         process.env.JWT_SECRET,
+         {expiresIn:"1d"}
+      );
+
+      res.status(200).json({
+         message:'Login successfully',
+         token
+      })
    } catch (error) {
       res.status(500).json({
          status:'fail',
