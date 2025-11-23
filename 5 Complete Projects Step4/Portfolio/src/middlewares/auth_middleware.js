b6a3923a4fbbc1bb02f486a30dfac62d265d2511
SalmanHossen <hossainsalman873@gmail.com>
2025-11-23 11:44:09 +0600

    Create auth middleware

---
+++ 5 Complete Projects Step4/Portfolio/src/middlewares/auth_middleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddleware=(req,res,next)=>{
+   try {
+      const token=req.headers.authorization?.split(' ')[1];
+      console.log('token: ',token);
+
+      if(!token){
+         return res.status(401).json({
+            message:'Token missing'
+         })
+      };
+
+      const decoded=jwt.verify(token,process.env.JWT_SECRET);
+      req.userId=decoded.id;
+
+      next();
+
+   } catch (error) {
+      res.status(500).json({
+         message:'Invalid token',
+         error:error.message
+      })
+   }
+}
+
+export default authMiddleware;
--- 5 Complete Projects Step4/Portfolio/src/models/User.model.js
+++ 5 Complete Projects Step4/Portfolio/src/models/User.model.js
@@ -11,7 +11,6 @@ const userSchema =new mongoose.Schema(
       required: [true, "Email is required"],
       unique: true,
       lowercase: true,
-      validate: [validator.isEmail, "Invalid email"],
     },
     password:{
       type: String,
