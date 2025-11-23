275ef30ed380b5ad49401d51f05da7f61c268055
SalmanHossen <hossainsalman873@gmail.com>
2025-11-23 12:10:18 +0600

    create user.model.js

--- 5 Complete Projects Step4/Portfolio/src/middlewares/auth_middleware.js
+++ 5 Complete Projects Step4/Portfolio/src/middlewares/auth_middleware.js
@@ -13,6 +13,7 @@ const authMiddleware=(req,res,next)=>{
 
       const decoded=jwt.verify(token,process.env.JWT_SECRET);
       req.userId=decoded.id;
+      req.userRole=decoded.role;
 
       next();
 
--- 5 Complete Projects Step4/Portfolio/src/models/User.model.js
+++ 5 Complete Projects Step4/Portfolio/src/models/User.model.js
@@ -1,47 +1,57 @@
 import mongoose from "mongoose";
 
-const userSchema =new mongoose.Schema(
-  {
-    name:{ 
-      type: String, 
-      required: [true, "Name is required"] 
-   },
-    email:{
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-      lowercase: true,
-    },
-    password:{
-      type: String,
-      required: [true, "Password is required"],
-      minlength: 6,
-    },
-    role:{ 
-      type: String, 
-      enum: ["admin", "client"], 
-      default: "client" 
-   },
-    avatarUrl:{
-      type:String,
-      default:""
-   },
-    bio: String,
-    isEmailVerified:{
-      type: Boolean, 
-      default: false 
-   },
-   resetPasswordToken:{
-      type:String,
-      default:null,
-   },
-   resetPasswordExpired:{
-      type:Date,
-      default:null
-   },
-    refreshTokens: [String], // stores hashed refresh tokens for revocation
+const userSchema=new mongoose.Schema({
+  name:{
+    type:String,
+    required:[true,"Name is required"],
+    trim:true,
   },
-  { timestamps: true }
-);
+  email:{
+    type:String,
+    required:[true,"Email is required"],
+    unique:true,
+    lowercase:true,
+    trim:true,
+  },
+  password:{
+    type:String,
+    required:[true,"Password is required"],
+    minlength:6
+  },
+  role:{
+    type:String,
+    enum:["admin","client"],
+    default:"client"
+  },
+  avatar_Url:{
+    type:String,
+    default:"",
+  },
+  bio:{
+    type:String,
+    default:""
+  },
+  isEmailVerified:{
+    type:String,
+    default:null
+  },
+  emailOTP:{
+    type:String,
+    default:'null'//hashed otp
+  },
+  emailOTPExpire:{
+    type:Date,
+    default:null,
+  },
+  resetPasswordToken:{
+    type:String,
+    default:null
+  },
+  resetPasswordExpired:{
+    type:String,
+    default:null
+  },
+  refreshTokens:[String],
+},{timestamps:true});
 
-export default mongoose.model('User',userSchema);
+export default mongoose.model("User",userSchema);
