0fb3aef70c36a7bf826173d82c574311a74febdc
SalmanHossen <hossainsalman873@gmail.com>
2025-11-25 16:01:41 +0600

    create multer.config.js and finish

---
+++ 5 Complete Projects Step4/Portfolio/src/config/multer.config.js
@@ -0,0 +1,16 @@
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinary.config.js";
+
+
+const storage=new CloudinaryStorage({
+   cloudinary,
+   params:{
+      folder:"uploads",
+      allowed_formats:['jpg','png','jpeg','webp'],
+   }
+});
+
+
+const upload=multer({storage});
+
+export default upload;
