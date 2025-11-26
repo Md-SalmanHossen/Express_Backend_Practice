8f61baba0157910d803cb27c3617b74c29898545
SalmanHossen <hossainsalman873@gmail.com>
2025-11-27 00:24:33 +0600

    create and multer as a middleware

---
+++ 5 Complete Projects Step4/Portfolio/src/middlewares/multer.config.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import 
+
+// storage settings
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads",            // Cloudinary folder
+    allowed_formats: ["jpg", "png", "jpeg"],
+    transformation: [{ width: 500, height: 500, crop: "fill" }] // auto resize
+  }
+});
+
+const upload = multer({ storage });
+
+export default upload;
