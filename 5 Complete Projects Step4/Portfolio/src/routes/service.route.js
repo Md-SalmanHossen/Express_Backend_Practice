6c68ece375255ecb41a603a2ed0dcad4a5a3e0fa
SalmanHossen <hossainsalman873@gmail.com>
2025-11-27 21:41:03 +0600

    add multer in service section

--- 5 Complete Projects Step4/Portfolio/src/routes/service.route.js
+++ 5 Complete Projects Step4/Portfolio/src/routes/service.route.js
@@ -1,5 +1,6 @@
 import *as service from '../controllers/service.controller.js';
 import auth from '../middlewares/auth_middleware.js';
+import upload from '../middlewares/multer.config.js';
 
 import express from 'express';
 
@@ -7,8 +8,8 @@ const router=express.Router();
 
 router.get('/', service.listAllService);//public
 
-router.post('/', auth , service.createService);//admin
-router.put('/:id', auth , service.updateService);
+router.post('/', auth ,upload.single('image'), service.createService);//admin
+router.put('/:id', auth , upload.single('image'), service.updateService);
 router.delete('/:id', auth ,service.deleteService);
 
 export default router;
